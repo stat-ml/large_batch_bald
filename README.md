620f1430084772e97aaa33c50810076db7601cf6
DahaKot <dascha-ist2013@ya.ru>
2022-05-18 17:20:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# BatchBALD Redux
+> Clean reimplementation of \"BatchBALD: Efficient and Diverse Batch Acquisition for Deep Bayesian Active Learning\"
+
+
+For an introduction & more information, see  http://batchbald.ml/. The paper can be found at http://arxiv.org/abs/1906.08158.
+
+The original implementation used in the paper is available at https://github.com/BlackHC/BatchBALD.
+
+We are grateful for fastai's [nbdev](https://nbdev.fast.ai/) which is powering this package.
+
+For more information, explore the sections and notebooks in the left-hand menu.
+The code is available on https://github.com/BlackHC/batchbald_redux, 
+and the website on https://blackhc.github.io/batchbald_redux.
+
+## Install
+
+`pip install batchbald_redux`
+
+## Motivation
+
+BatchBALD is an algorithm and acquisition function for Active Learning in a Bayesian setting using BNNs and MC dropout.
+
+The aquisition function is the mutual information between the joint of a candidate batch and the model parameters $\omega$:
+
+{% raw %}
+$$a_{\text{BatchBALD}}((y_b)_B) = I[(y_b)_B;\omega]$$
+{% endraw %}
+
+The best candidate batch is one that maximizes this acquisition function. 
+
+In the paper, we show that this function satisfies sub-modularity, which provides us an optimality guarantee for a greedy algorithm. The candidate batch is selected using greedy expansion.
+
+Joint entropies are hard to estimate and, for everything to work, one also has to use consistent MC dropout, which keeps a set of dropout masks fixed while scoring the pool set.
+
+To aid reproducibility and baseline reproduction, we provide this simpler and clearer reimplementation.
+
+## Please cite us
+
+```
+@misc{kirsch2019batchbald,
+    title={BatchBALD: Efficient and Diverse Batch Acquisition for Deep Bayesian Active Learning},
+    author={Andreas Kirsch and Joost van Amersfoort and Yarin Gal},
+    year={2019},
+    eprint={1906.08158},
+    archivePrefix={arXiv},
+    primaryClass={cs.LG}
+}
+```
+
+## How to use
+
+We provide a simple example experiment that uses this package [here](https://blackhc.github.io/batchbald_redux/example_experiment/). 
+
+To get a candidate batch using BatchBALD, we provide a simple API in [`batchbald_redux.batchbald`](https://blackhc.github.io/batchbald_redux/batchbald/):
+
+
+<h4 id="get_batchbald_batch" class="doc_header"><code>get_batchbald_batch</code><a href="https://github.com/blackhc/batchbald_redux/tree/master/batchbald_redux/batchbald.py#L67" class="source_link" style="float:right">[source]</a></h4>
+
+> <code>get_batchbald_batch</code>(**`log_probs_N_K_C`**:`Tensor`, **`batch_size`**:`int`, **`num_samples`**:`int`, **`dtype`**=*`None`*, **`device`**=*`None`*)
+
+
+
+
+We also provide a simple implementation of consistent MC dropout in [`batchbald_redux.consistent_mc_dropout`](https://blackhc.github.io/batchbald_redux/consistent_mc_dropout/).
---
+++ cnn_models.py
@@ -0,0 +1,148 @@
+import torch
+from torch import nn as nn
+from torch.nn import functional as F
+from batchbald_redux import consistent_mc_dropout
+
+class CNN_MC_RMNIST(consistent_mc_dropout.BayesianModule):
+    def __init__(self, num_classes=10):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.conv1_drop = consistent_mc_dropout.ConsistentMCDropout2d()
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
+        self.conv2_drop = consistent_mc_dropout.ConsistentMCDropout2d()
+        self.fc1 = nn.Linear(1024, 128)
+        self.fc1_drop = consistent_mc_dropout.ConsistentMCDropout()
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def mc_forward_impl(self, input: torch.Tensor):
+        input = F.relu(F.max_pool2d(self.conv1_drop(self.conv1(input)), 2))
+        input = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(input)), 2))
+        input = input.view(-1, 1024)
+        input = F.relu(self.fc1_drop(self.fc1(input)))
+        input = self.fc2(input)
+        input = F.log_softmax(input, dim=1)
+
+        return input
+    
+class CNN_ENS_RMNIST(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.conv1_drop = nn.Dropout(p=0.25)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
+        self.conv2_drop = nn.Dropout(p=0.25)
+        self.fc1 = nn.Linear(1024, 128)
+        self.fc1_drop = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, input: torch.Tensor):
+        input = F.relu(F.max_pool2d(self.conv1_drop(self.conv1(input)), 2))
+        input = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(input)), 2))
+        input = input.view(-1, 1024)
+        input = F.relu(self.fc1_drop(self.fc1(input)))
+        input = self.fc2(input)
+        input = F.log_softmax(input, dim=1)
+
+        return input
+    
+class CNN_MC_EMNIST(consistent_mc_dropout.BayesianModule):
+    def __init__(self, num_classes=47):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv1_drop = consistent_mc_dropout.ConsistentMCDropout2d()
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv2_drop = consistent_mc_dropout.ConsistentMCDropout2d()
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv3_drop = consistent_mc_dropout.ConsistentMCDropout2d()
+        self.fc1 = nn.Linear(128 * 4 * 4, 512)
+        self.fc1_drop = consistent_mc_dropout.ConsistentMCDropout()
+        self.fc2 = nn.Linear(512, num_classes)
+
+    def mc_forward_impl(self, input:  torch.Tensor):
+        input = F.relu(F.max_pool2d(self.conv1_drop(self.conv1(input)), 2))
+        input = F.relu(self.conv2_drop(self.conv2(input)))
+        input = F.relu(F.max_pool2d(self.conv3_drop(self.conv3(input)), 2))
+        input = input.view(-1, 128 * 4 * 4)
+        input = F.relu(self.fc1_drop(self.fc1(input)))
+        input = self.fc2(input)
+        input = F.log_softmax(input, dim=1)
+        return input
+    
+class CNN_ENS_EMNIST(nn.Module):
+    def __init__(self, num_classes=47):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv1_drop = nn.Dropout(p=0.25)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv2_drop = nn.Dropout(p=0.25)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv3_drop = nn.Dropout(p=0.25)
+        self.fc1 = nn.Linear(128 * 4 * 4, 512)
+        self.fc1_drop = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(512, num_classes)
+        self.relu = nn.ReLU(inplace=True)
+        self.max_pool2d = nn.MaxPool2d(kernel_size=2)
+
+    def forward(self, input: torch.Tensor):
+        input = F.relu(self.max_pool2d(self.conv1_drop(self.conv1(input))))
+        input = F.relu(self.conv2_drop(self.conv2(input)))
+        input = F.relu(self.max_pool2d(self.conv3_drop(self.conv3(input))))
+        input = input.view(-1, 128 * 4 * 4)
+        input = F.relu(self.fc1_drop(self.fc1(input)))
+        input = self.fc2(input)
+        input = F.log_softmax(input, dim=1)
+        return input
+    
+# model = nn.Sequential( # (batch_size, 3, 32, 32)
+#         nn.Conv2d(3, 32, kernel_size=3), # (batch_size, 32, 30, 30)
+#         nn.ReLU(inplace=True),
+#         nn.Dropout(p=0.25),
+#         nn.Conv2d(32, 32, kernel_size=3), # (batch_size, 32, 28, 28)
+#         nn.ReLU(inplace=True),
+#         nn.Dropout(p=0.25),
+#         nn.MaxPool2d(kernel_size=2), # (batch_size, 64, 14, 14)
+#         nn.Conv2d(32, 64, kernel_size=3), # (batch_size, 64, 12, 12)
+#         nn.ReLU(inplace=True),
+#         nn.Dropout(p=0.25),
+#         nn.Conv2d(64, 64, kernel_size=3), # (batch_size, 64, 10, 10)
+#         nn.ReLU(inplace=True),
+#         nn.Dropout(p=0.25),
+#         nn.MaxPool2d(kernel_size=2), # (batch_size, 64, 5, 5)
+#         nn.Flatten(), # (batch_size, 64*5*5)
+#         nn.Dropout(p=0.5),
+#         nn.Linear(64*5*5, 10), # (batch_size, )
+#         )
+    
+class CNN_ENS_CIFAR10(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3)
+        self.conv1_drop = nn.Dropout(p=0.25)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3)
+        self.conv2_drop = nn.Dropout(p=0.25)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3_drop = nn.Dropout(p=0.25)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=3)
+        self.conv4_drop = nn.Dropout(p=0.25)
+        
+        self.fc1_drop = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(64*5*5, num_classes)
+        
+        self.relu = nn.ReLU(inplace=True)
+        self.max_pool2d = nn.MaxPool2d(kernel_size=2)
+
+    def forward(self, input: torch.Tensor):
+        input = self.conv1_drop(self.relu(self.conv1(input)))
+        input = self.max_pool2d(self.conv2_drop(self.relu(self.conv2(input))))
+        input = self.conv3_drop(self.relu(self.conv3(input)))
+        input = self.max_pool2d(self.conv4_drop(self.relu(self.conv4(input))))
+        input = input.view(-1, 64 * 5 * 5)
+        input = self.fc1_drop(self.relu(input))
+        input = self.fc2(input)
+        input = F.log_softmax(input, dim=1)
+        return input
